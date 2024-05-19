a85c46051190bb597649ddaeb7e007d4ecfb332a
amir <admin@groupeffect.de>
2024-05-20 00:39:00 +0200

    wip

--- webinterface/vite.config.js
+++ webinterface/vite.config.js
@@ -1,10 +1,24 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'node:url'
+// import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  define: {
+    'process.env': process.env,
+    
+  },
   plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
   build: {
     outDir: "../docs"
   },
+  base: process.env.NODE_ENV === 'production'
+  ? '/'
+  : '/'
 })
